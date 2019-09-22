1709cb2c28efa4b6ec2939b8116e792a0c4a45ff
armour <250315807@qq.com>
2019-09-22 16:06:25 +0800

    注释

--- src/test/java/com/zero/TestDao.java
+++ src/test/java/com/zero/TestDao.java
@@ -7,8 +7,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import com.zero.blog.dao.LinkDao;
 import com.zero.blog.dao.UserDao;
+import com.zero.blog.entity.Link;
 import com.zero.blog.entity.User;
 
 @RunWith(SpringRunner.class)
@@ -17,6 +18,8 @@ public class TestDao {
 
 	@Autowired
 	private UserDao userDao;
+	@Autowired
+	private LinkDao linkDao;
 	
 	@Test
 	public void testUserDao() {
@@ -26,4 +29,34 @@ public class TestDao {
 		}
 		System.out.println(userDao.getRowCount(""));
 	}
+	@Test
+	public void testLinkDao() {
+		List<Link> list = linkDao.findAll();
+		for(Link link:list) {
+			System.out.println(link);
+		}
+	}
+	@Test
+	public void testDeleteLinkById() {
+		int row = linkDao.deleteLinkById(2);
+		System.out.println("被影响的行数："+row);
+	}
+	
+	@Test
+	public void testInsertLink() {
+		int row = linkDao.insertLink(new Link(null,"新浪","www.sina.com","11"));
+		System.out.println("被影响的行数："+row);
+	}
+	@Test
+	public void testFindLinkById() {
+		List<Link> list = linkDao.findLinkById(2);
+		System.out.println(list);
+	}
+	
+	@Test
+	public void testUpdate() {
+		int row = linkDao.updateLinkById(new Link(2,"新浪","www.sina.com","新浪.jpg"));
+		System.out.println("被影响的行数："+row);
+	}
+	
 }
